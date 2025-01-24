92749afa9ffa586a99c1ecc548ed1420ae2938aa
hritiksarraf <hritiksarraf@gmail.com>
2025-01-24 19:17:12 +0530

    booking

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -97,7 +97,7 @@ export default function Navbar() {
                                 </div>
                                 <h1 className="text-white font-bold">{user.name}</h1>
                                 <div className="flex flex-col gap-5">
-                                    <Link href="/bookings" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-[#F5AA2B] hover:bg-yellow-400 active:bg-[#0E2041] rounded-full md:inline-flex">
+                                    <Link href="/yourOrder" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-[#F5AA2B] hover:bg-yellow-400 active:bg-[#0E2041] rounded-full md:inline-flex">
                                         Bookings
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                             <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
@@ -236,7 +236,7 @@ export default function Navbar() {
                             </div>
                             <h1 className="text-white font-bold">{user.name}</h1>
                             <div className="flex flex-col gap-5">
-                                <Link href="/bookings" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-[#F5AA2B] hover:bg-yellow-400 active:bg-blue-600 rounded-full md:inline-flex">
+                                <Link href="/yourOrder" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-[#F5AA2B] hover:bg-yellow-400 active:bg-blue-600 rounded-full md:inline-flex">
                                     Bookings
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                         <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
